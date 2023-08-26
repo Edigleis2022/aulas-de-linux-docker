9ff95cb6208ca2d4fc8e6fc8032b63532868ea3f
Edigleis <105567136+Edigleis2022@users.noreply.github.com>
2023-08-25 21:42:15 -0400

    ⚡️ Comandos Docker no Linux

--- introducao-docker/script.sh
+++ introducao-docker/script.sh
@@ -1,3 +1,5 @@
+##### COMANDOS BASICOS ####
+
 # Hello World
 docker run hello-world # Criar um container de forma persistente
 docker run --rm hello-world # Criar um container temporário
@@ -7,19 +9,19 @@ docker container list #Comando que lista todos os containers em execução
 docker ps -a # Comando que lista todos os containers, mesmo parados
 
 #Lista imagens offline (cache do docker hub)
-docker image list
+docker imdocker run hello-world # Criar um containerage list
 
 #Comandos de gerenciamento de estado dos conteiners
 docker start NOME_DOC_CONTAINER ou ID_DO_CONTAINER #Iiniciar
 docker stop NOME_DO_CONTAINER ou ID_DO_ CONTAINER # Parar
-docker restart NOME_DO_CONTAINER ou ID_DO_CONTAINER # Reiniciar
+docker restar##### COMANDOS BASICOS ####t NOME_DO_CONTAINER ou ID_DO_CONTAINER # Reiniciar
 docker rm NOME_DO_CONTAINER ou ID_DO_CONTAINER # Romover
 
 #Comandos de gerenciamento de estado dos conteiners
 docker start $(docker ps -a -q) #Iiniciar
 docker stop $(docker ps -a -q) # Parar
 docker restart $(docker ps -a -q) # Reiniciar
-docker rm $(docker ps -a -q) # Romover
+docker rm $(docker ps -a -q) # Remover
 
 #Comando em lote para gerenciar container a partir dos IDs
 docker stop $(docker ps -a -q) # Iniciar, Parar, Reiniciar, Remover
@@ -27,3 +29,7 @@ docker stop $(docker ps -a -q) # Iniciar, Parar, Reiniciar, Remover
 
 #Zerar o docker 
 docker ssytem prune --all -f
+
+
+##### CONTAINER COM EXECUÇAO CONSTANTE #####
+docker run --name nginx -p 80:80 -d nginx:1.25-alpine
